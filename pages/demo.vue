d1d8c7f1beabf8ea944aa75bec39ec141f4e534b
Civan Erbay <info@civan-erbay.de>
2023-12-20 01:34:04 +0100

    Setup basic UI

---
+++ pages/demo.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="bg-gray-900 text-white min-h-screen flex flex-col justify-center items-center">
+    <!-- Dropdowns and Submit Section -->
+    <section class="container mx-auto py-16">
+      <div class="grid grid-cols-1 gap-4 text-center">
+        <!-- Country Dropdown -->
+        <select v-model="selectedCountry" class="bg-gray-800 rounded-lg p-2 text-gray-300">
+          <option disabled value="">Select Country</option>
+          <option>Germany</option>
+        </select>
+
+        <!-- Region Dropdown -->
+        <select v-model="selectedRegion" class="bg-gray-800 rounded-lg p-2 text-gray-300">
+          <option disabled value="">Select Region</option>
+          <option v-for="region in regions" :key="region">{{ region }}</option>
+        </select>
+
+        <!-- School Type Dropdown -->
+        <select v-model="selectedSchoolType" @change="updateGrades" class="bg-gray-800 rounded-lg p-2 text-gray-300">
+          <option disabled value="">Select School Type</option>
+          <option v-for="type in schoolTypes" :key="type">{{ type }}</option>
+        </select>
+
+        <!-- Grade Dropdown -->
+        <select v-model="selectedGrade" class="bg-gray-800 rounded-lg p-2 text-gray-300">
+          <option disabled value="">Select Grade</option>
+          <option v-for="grade in grades" :key="grade">{{ grade }}</option>
+        </select>
+
+        <!-- Teaching Type Dropdown -->
+        <select v-model="selectedTeachingType" class="bg-gray-800 rounded-lg p-2 text-gray-300">
+          <option disabled value="">Select Teaching Type</option>
+          <option>Regulär</option>
+          <option>Reformpädagogisch</option>
+        </select>
+
+        <!-- Subject Dropdown -->
+        <select v-model="selectedSubject" class="bg-gray-800 rounded-lg p-2 text-gray-300">
+          <option disabled value="">Select Subject</option>
+          <option v-for="subject in subjects" :key="subject">{{ subject }}</option>
+        </select>
+
+        <!-- Submit Button -->
+        <button @click="submitForm" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Submit
+        </button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+
+const selectedCountry = ref('')
+const selectedRegion = ref('')
+const selectedSchoolType = ref('')
+const selectedGrade = ref('')
+const selectedTeachingType = ref('')
+const selectedSubject = ref('')
+const regions = ref(['Baden-Württemberg', 'Bavaria', 'Berlin', 'Brandenburg', 'Bremen', 'Hamburg', 'Hesse', 'Lower Saxony', 'Mecklenburg-Vorpommern', 'North Rhine-Westphalia', 'Rhineland-Palatinate', 'Saarland', 'Saxony', 'Saxony-Anhalt', 'Schleswig-Holstein', 'Thuringia'])
+const schoolTypes = ref(['Grundschule', 'Gymnasium', 'Realschule', 'Hauptschule', 'Gesamtschule', 'Sonderschule'])
+const subjects = ref(['German', 'English', 'Physics', 'Religion', 'Sports', 'Math', 'History', 'Biology', 'Chemistry', 'Geography', 'Art', 'Music', 'Computer Science'])
+const grades = ref([])
+
+const updateGrades = () => {
+  switch (selectedSchoolType.value) {
+    case 'Grundschule':
+      grades.value = Array.from({ length: 4 }, (_, i) => i + 1)
+      break
+    case 'Hauptschule':
+    case 'Realschule':
+    case 'Sonderschule':
+      grades.value = Array.from({ length: 6 }, (_, i) => i + 5)
+      break
+    case 'Gymnasium':
+    case 'Gesamtschule':
+      grades.value = Array.from({ length: 9 }, (_, i) => i + 5)
+      break
+    default:
+      grades.value = []
+  }
+}
+
+const submitForm = async () => {
+  const formData = {
+    country: selectedCountry.value,
+    region: selectedRegion.value,
+    schoolType: selectedSchoolType.value,
+    grade: selectedGrade.value,
+    teachingType: selectedTeachingType.value,
+    subject: selectedSubject.value
+  }
+  try {
+    const response = await axios.post('your-api-endpoint', formData)
+    console.log(response.data)
+    // Handle response
+  } catch (error) {
+    console.error(error)
+    // Handle error
+  }
+}
+</script>
+
+<style scoped>
+/* Tailwind CSS styles */
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="bg-gray-900 text-white min-h-screen flex flex-col justify-center items-center">
+    <!-- Header Section -->
+    <header class="text-center">
+      <h1 class="text-4xl font-bold mb-4">Welcome to Smarteach.ai</h1>
+      <p class="text-lg mb-8">Revolutionizing Education with AI</p>
+      <a href="#features" class="text-blue-500 hover:underline">Learn More</a>
+    </header>
+
+    <!-- Features Section -->
+    <section id="features" class="py-16">
+      <div class="container mx-auto text-center">
+        <h2 class="text-3xl font-bold mb-8">Our Features</h2>
+
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          <!-- Feature 1 -->
+          <div class="p-6 bg-gray-800 rounded-lg">
+            <i class="text-4xl text-blue-500 mb-4 fas fa-chalkboard-teacher"></i>
+            <h3 class="text-xl font-semibold mb-2">Smart Scheduling</h3>
+            <p class="text-gray-300">Create optimized teacher schedules based on educational sciences context.</p>
+          </div>
+
+          <!-- Feature 2 -->
+          <div class="p-6 bg-gray-800 rounded-lg">
+            <i class="text-4xl text-blue-500 mb-4 fas fa-rocket"></i>
+            <h3 class="text-xl font-semibold mb-2">AI-Powered Efficiency</h3>
+            <p class="text-gray-300">Boost efficiency and productivity with our AI-driven tools.</p>
+          </div>
+
+          <!-- Feature 3 -->
+          <div class="p-6 bg-gray-800 rounded-lg">
+            <i class="text-4xl text-blue-500 mb-4 fas fa-lightbulb"></i>
+            <h3 class="text-xl font-semibold mb-2">Innovative Solutions</h3>
+            <p class="text-gray-300">Bring innovative solutions to the field of education.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- CTA Section -->
+    <section class="bg-blue-500 py-16 text-center">
+      <h2 class="text-3xl font-bold text-white mb-4">Ready to Get Started?</h2>
+      <p class="text-lg text-white mb-8">Join us in shaping the future of education!</p>
+      <nuxt-link to="demo"
+        class="bg-white text-blue-500 hover:bg-blue-700 text-lg font-semibold py-2 px-6 rounded-full">Try for free</nuxt-link>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact" class="py-16">
+      <div class="container mx-auto text-center">
+        <h2 class="text-3xl font-bold mb-8">Contact Us</h2>
+        <p class="text-gray-300 mb-4">Have questions or want to learn more about Smarteach.ai? Get in touch with us!</p>
+        <a href="mailto:info@smarteach.ai" class="text-blue-500 hover:underline">info@smarteach.ai</a>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+}
+</script>
+
+<style scoped>
+/* You can customize your styles further here */
+</style>
