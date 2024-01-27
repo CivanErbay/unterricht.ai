a9102e273a5dcc9cdf307d02e0f98fda7e7ae583
Paco Muro <paco@studioborabora.com>
2024-01-27 18:13:02 +0100

    fixed bg

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <NuxtLayout>
     <div class="min-h-screen bg-blue-900/70">
       <div
-        class="absolute inset-0 -z-10 bg-[url('/public/img/teachers-room-bg.jpg')] bg-cover bg-center"
+        class="fixed inset-0 -z-10 bg-[url('/public/img/teachers-room-bg.jpg')] bg-cover bg-center"
       ></div>
       <Navbar />
       <NuxtPage class="pt-8" />
